d91903ae6656d74d1bc3e217912a082ebfdfa21e
Phuvis Kerdpramote <66050977@kmitl.ac.th>
2026-02-12 13:53:10 +0700

    added

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+while True:
+    user_input = input("What you gotta say? : ")
+    
+    if user_input == "STOP":
+        break
+    
+    user_input = input("I got that! Anything else? : ")
+    
+    if user_input == "STOP":
+        break
